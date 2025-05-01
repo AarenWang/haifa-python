a437d4d7427ce1f0ab730bb7e72c5ad01eed13ca
AarenWang <AarenWang@sina.com>
2025-05-01 21:54:51 +0800

    register interpreter recursion

--- vm/test_register_vm_v3.py
+++ vm/test_register_vm_v3.py
@@ -34,6 +34,68 @@ class TestInterpreterVM(unittest.TestCase):
         interpreter = Interpreter(script)
         output = interpreter.run()
         self.assertEqual(output, [3, 4])
+        
+    def test_recursion_factorial(self):
+        script = [
+            "FUNC fact",
+            "ARG n",
+            "MOV one 1",
+            "LT cond n one",
+            "JZ cond recurse",
+            "MOV ret 1",
+            "RETURN ret",
+            "LABEL recurse",
+            "SUB n1 n one",
+            "PARAM n1",
+            "CALL fact",
+            "RESULT res",
+            "MUL ret res n",
+            "RETURN ret",
+            "ENDFUNC",
+            "MOV x 5",
+            "PARAM x",
+            "CALL fact",
+            "RESULT y",
+            "PRINT y"
+        ]
+        interpreter = Interpreter(script)
+        output = interpreter.run()
+        print(f"Output: {output}")
+        self.assertEqual(output, [120])
+        
+    def test_recursive_fibonacci(self):
+        script = [
+            "FUNC fib",
+            "ARG n",
+            "MOV two 2",
+            "LT cond n two",
+            "JZ cond recurse",
+            "MOV ret n",
+            "RETURN ret",
+            "LABEL recurse",
+            "MOV one 1",
+            "SUB n1 n one",
+            "SUB n2 n two",
+            "PARAM n1",
+            "CALL fib",
+            "RESULT r1",
+            "PARAM n2",
+            "CALL fib",
+            "RESULT r2",
+            "ADD ret r1 r2",
+            "RETURN ret",
+            "ENDFUNC",
+            "MOV x 7",
+            "PARAM x",
+            "CALL fib",
+            "RESULT y",
+            "PRINT y"
+        ]
+        interpreter = Interpreter(script)
+        output = interpreter.run()
+        print(f"Output: {output}")
+        self.assertEqual(output, [13])  
+            
 
 if __name__ == '__main__':
     unittest.main()
